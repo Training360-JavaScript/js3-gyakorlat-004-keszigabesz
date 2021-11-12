899a6b62a88f4b94c9110656ba7b36d286edd4af
keszigabesz <bajo.gabor@gmail.com>
2021-11-12 18:25:04 +0100

    első három kész

--- solutions/mac.js
+++ solutions/mac.js
@@ -1,5 +1,5 @@
 const checkMac = macAddress => {
-    const macAddressRegex = /^$/;
+    const macAddressRegex = /^([0-9a-f]{2}[:-]){5}[0-9a-f]{2}$/i;
     return macAddress.match(macAddressRegex) ? true : false;
 }
 
